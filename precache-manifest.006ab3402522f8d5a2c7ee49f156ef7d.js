dd01632c46fc9a82711b4cc42d6865e44655f684
ShariyarAbeer <007abeershariyar@gmail.com>
2019-04-28 00:46:48 +0600

    Updates

---
+++ precache-manifest.006ab3402522f8d5a2c7ee49f156ef7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "099f519cab59f1696f26",
+    "url": "/ReactSampleWebpage.git/static/js/runtime~main.24072cd3.js"
+  },
+  {
+    "revision": "44b4a82d68d14424ecdf",
+    "url": "/ReactSampleWebpage.git/static/js/main.d8a4ec1c.chunk.js"
+  },
+  {
+    "revision": "a13e116ce43f8254531e",
+    "url": "/ReactSampleWebpage.git/static/js/2.15f70701.chunk.js"
+  },
+  {
+    "revision": "44b4a82d68d14424ecdf",
+    "url": "/ReactSampleWebpage.git/static/css/main.44d65472.chunk.css"
+  },
+  {
+    "revision": "2eea4b97429e3add051ea8fceab50234",
+    "url": "/ReactSampleWebpage.git/index.html"
+  }
+];
